a1a00fc36a0e8041813ceb77d5836c346ac0deb2
Duncan Mainya <duncanmainya2020@gmail.com>
2025-11-09 13:33:32 +0300

    Update inference

--- inference.py
+++ inference.py
@@ -2,14 +2,14 @@
 from fastai.collab import *
 from fastai.tabular.all import *
 from fastai.learner import load_learner
-import pickle
 
 # Loading the model
-learn_inf = load_learner(r"C:\Users\Dan\Desktop\Movielens\model\PMF_collab_model_1.pkl", pickle_module=pickle)
+model_path = Path("/home/duncan/fastai-project/venv/model/PMF_collab_model_1.pkl")
+learn_inf = load_learner(model_path)
 
 # Reading relevant files
-ratings = pd.read_csv(r"C:\Users\Dan\Desktop\Movielens\data\ratings.csv", sep=",")
-movies = pd.read_csv(r"C:\Users\Dan\Desktop\Movielens\data\movies.csv", sep=",")
+ratings = pd.read_csv("/home/duncan/fastai-project/venv/data/ratings.csv", sep=",")
+movies = pd.read_csv("/home/duncan/fastai-project/venv/data/movies.csv", sep=",")
 ratings = ratings.merge(movies)
 
 # Turning csv into dataframe
@@ -19,7 +19,9 @@ ratings = pd.DataFrame(ratings)
 dls = CollabDataLoaders.from_df(ratings, item_name='title', bs=64)
 
 # Creating user input
-user_input = input("Enter movie name: ")
+
+print("\n=====================================================\n")
+user_input = input("\nEnter movie name: ")
 
 # Finding movies with similar embedding distances
 movie_name = f"{user_input}"
@@ -27,6 +29,7 @@ movie_factors = learn_inf.model.movie_factors.weight
 idx = dls.classes['title'].o2i[movie_name]
 distances = nn.CosineSimilarity(dim=1)(movie_factors, movie_factors[idx][None])
 idx = distances.argsort(descending=True)[1:6]
-print(f"Users who Liked {movie_name} also liked:")
+print(f"\nUsers who Liked {movie_name} also liked:")
 for t in dls.classes['title'][idx]:
     print(f"\n{t}")
+print("\n=====================================================\n")
